7cd94fb26c8dd758ff87a0b1bb675cf13ae926db
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-27 00:22:46 +0200

    1

--- src/components/HomePage/HomePage.styled.js
+++ src/components/HomePage/HomePage.styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import * as palette from '../../variables/Variables';
 import { Button } from '../../styles/Button.styled';
